569bb90f7f9cb0580e74783c93ca62039c22336e
Camilo López <camiloj.lopez79@gmail.com>
2022-05-12 19:07:14 -0400

    Se crea el controlador y modelo de usuario

--- src/main/java/com/cursojava/curso/controllers/UserController.java
+++ src/main/java/com/cursojava/curso/controllers/UserController.java
@@ -1,12 +1,65 @@
 package com.cursojava.curso.controllers;
 
+import java.util.List;
+// Models
+import com.cursojava.curso.models.User;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-  @RequestMapping( value = "test" )
-  public String test() {
-    return "Test";
+  @RequestMapping( value = "user/{id}" )
+  public User getUser( @PathVariable Long id ) {
+    User user = new User();
+    // Set Attributes
+    user.setId( id );
+    user.setName( "Camilo" );
+    user.setLastName( "Completo" );
+    user.setEmail( "camilo.completo@italiano.com" );
+    user.setPhone( "+5696655443" );
+    user.setPassword( "123456" );
+
+    return user;
+  }
+
+  @RequestMapping( value = "user22" )
+  public User updateUser() {
+    User user = new User();
+    // Set Attributes
+    user.setName( "Camilo" );
+    user.setLastName( "Completo" );
+    user.setEmail( "camilo.completo@italiano.com" );
+    user.setPhone( "+5696655443" );
+    user.setPassword( "123456" );
+
+    return user;
+  }
+
+  @RequestMapping( value = "user33" )
+  public User deleteUser() {
+    User user = new User();
+    // Set Attributes
+    user.setName( "Camilo" );
+    user.setLastName( "Completo" );
+    user.setEmail( "camilo.completo@italiano.com" );
+    user.setPhone( "+5696655443" );
+    user.setPassword( "123456" );
+
+    return user;
+  }
+
+  @RequestMapping( value = "user44" )
+  public User getUsers() {
+    User user = new User();
+    // Set Attributes
+    user.setName( "Camilo" );
+    user.setLastName( "Completo" );
+    user.setEmail( "camilo.completo@italiano.com" );
+    user.setPhone( "+5696655443" );
+    user.setPassword( "123456" );
+
+    return user;
   }
 }
