bc00af68e9545b41c6160b384e422ba47a8862cf
Camilo López <camiloj.lopez79@gmail.com>
2022-05-13 10:05:39 -0400

    Se muestra data en duro en html

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 const chargeUsers = async () => {
-  const request = await fetch( 'users/1243', {
+  const request = await fetch( 'users', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -14,5 +14,27 @@ const chargeUsers = async () => {
   });
 
   const users = await request.json();
-  console.log( users );
+  console.log( users )
+  let htmlList = '';
+
+  for ( let user of users ) {
+    let userHTML = `
+      <tr>
+        <td>${ user.id }</td>
+        <td>${ user.name }</td>
+        <td>${ user.lastName }</td>
+        <td>${ user.email }</td>
+        <td>${ user.phone }</td>
+        <td>
+          <a href="#" class="btn btn-danger btn-circle btn-sm">
+            <i class="fas fa-trash"></i>
+          </a>
+        </td>
+      </tr>
+    `;
+
+    htmlList += userHTML;
+  }
+
+  document.querySelector( '#users tbody' ).outerHTML = htmlList;
 }
